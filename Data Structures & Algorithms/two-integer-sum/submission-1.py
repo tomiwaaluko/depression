a03c14b59c2b57aa7aee4c940269608113c6823d
Tomiwa Aluko <112286524+tomiwaaluko@users.noreply.github.com>
2026-08-01 23:23:07 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+
+        for i, n in enumerate(nums):
+            diff  = target - nums[i]
+
+            if diff in hmap:
+                return [hmap[diff], i]
+            hmap[n] = i
+               
+                
+
