c3c4814694883ce4a7a726488b13f18ced747a46
Tomiwa Aluko <112286524+tomiwaaluko@users.noreply.github.com>
2026-08-28 00:03:26 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count1 = {}
+        count2 = {}
+
+        for i in range(len(s)):
+            count1[s[i]] = 1 + count1.get(s[i], 0)
+            count2[t[i]] = 1 + count2.get(t[i], 0)
+        
+        if count1 == count2:
+            return True
+        else:
+            return False
+
+        
+        
